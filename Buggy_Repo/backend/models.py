c0ab73369d959dc2e5b68fa6fb880b8d308cf8f9
vineelsaireddy123 <vineelsaireddys@gmail.com>
2025-04-16 16:37:07 +0530

    models.py

--- Buggy_Repo/backend/models.py
+++ Buggy_Repo/backend/models.py
@@ -1,11 +1,9 @@
 from pydantic import BaseModel
 
-class Item():
-    name: int
+class Item(BaseModel):
+    name: str
     description: str
 
 class User(BaseModel):
     username: str
     bio: str
-    
-    # You can raise your hands and give the answer to the chocolate question
