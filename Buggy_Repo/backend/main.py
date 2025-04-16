a3704eb1f68f48d4c21d45fc697fef92bedb8155
vineelsaireddy123 <vineelsaireddys@gmail.com>
2025-04-16 16:34:59 +0530

    main.py

--- Buggy_Repo/backend/main.py
+++ Buggy_Repo/backend/main.py
@@ -6,10 +6,9 @@ from routes.quiz import router as quiz_router
 app = FastAPI()
 
 app.include_router(items_router, prefix="/items")
-app.include_router(analytics_router)
-app.include_router(quiz_router)
+app.include_router(analytics_router, prefix="/analytics")
+app.include_router(quiz_router, prefix="/quiz")
 
-# why the hell did I write this function?
 @app.get("/home")
 async def get_home():
     return {"message": "Welcome to the Multi-Page FastAPI App!"}
